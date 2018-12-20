0766d90af3e6a724bb82bbc880d3f82a8fd99bc7
Code Master <codemaster08240328@gmail.com>
2018-12-21 03:08:09 +1000

    feature: frontend structure..

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
+from django.conf.urls import url, include
+from django.views import generic
+from . import views
+from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', generic.RedirectView.as_view(
+         url='/api/', permanent=False)),
+    url(r'^api/$', get_schema_view()),
+    url(r'^api/auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/auth/token/obtain/$', TokenObtainPairView.as_view()),
+    url(r'^api/auth/token/refresh/$', TokenRefreshView.as_view()),
+    url(r'^api/echo/$', views.EchoView.as_view())
 ]
---
+++ backend/config/views.py
@@ -0,0 +1,12 @@
+from rest_framework import views, serializers, status
+from rest_framework.response import Response
+
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField()
+
+class EchoView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = MessageSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED)
