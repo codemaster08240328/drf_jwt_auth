60a8a5e3c4e159ccae96f08a0382ba8dbe33e620
Code Master <codemaster08240328@gmail.com>
2018-12-21 20:43:39 +1000

    feature: add login form..

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+import {Route, Switch, Router} from 'react-router'
+import Login from './containers/Login';
+import PrivateRoute from './containers/PrivateRoute';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import configureStore from './store'
+const history = createHistory()
+const store = configureStore(history)
+ReactDOM.render((
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/login/" component={Login} />
+        <PrivateRoute path="/" component={App}/>
+      </Switch>
+    </Router>
+  </Provider>
+  
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
